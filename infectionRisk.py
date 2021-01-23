a6b91a6a8ffce0173e9b7bb301176ff18b641edd
Divya Thakkar <thakkd2@mcmaster.ca>
2021-01-23 15:45:48 -0500

    updated infection calc to match dictionary

--- infectionRisk.py
+++ infectionRisk.py
@@ -1,7 +1,3 @@
-# will need to adjust for the fact that multiple possible infectors could be sitting in a classroom, could be travelling to the classroom
-
-
-
 # Calculating Percent of Risk Going Forward
 # I think This needs to be recalculated at each stage 
 def calculateInfectionRisk(checkedStudent, classList):
@@ -9,19 +5,18 @@ def calculateInfectionRisk(checkedStudent, classList):
     filteredInfectedList = [chance for chance in classList if chance > 0]
 
     for student in filteredInfectedList:
-        givenRisk += student.infectorChance*(3.0/len(classList))
+        checkedStudent[givenRisk] += student[givenRisk]*(3.0/len(classList))
 
-    if(checkedStudent.healthFlag == 1):
-        givenRisk *= 1.7
+    if(checkedStudent[healthFlag] == 1):
+        checkedStudent[givenRisk] *= 1.7
         
-    if(checkedStudent.grade == 12):
-        givenRisk *= 1.75
-    elif(checkedStudent.grade == 11):
-        givenRisk *= 1.5
-    elif(checkedStudent.grade == 10):
-        givenRisk *= 1.25
-    
-    return givenRisk
+    if(checkedStudent[grade] == 12):
+        checkedStudent[givenRisk] *= 1.75
+    elif(checkedStudent[grade] == 11):
+        checkedStudent[givenRisk] *= 1.5
+    elif(checkedStudent[grade] == 10):
+        checkedStudent[givenRisk] *= 1.25
+    # given Risk for the student has been updated and does not need to be updated
 
 
 # SAMPLE DATA FOR TESTING
