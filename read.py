17f68f633065c1ace6ff91ddb3d7d2e205ac7b7f
Divya Thakkar <thakkd2@mcmaster.ca>
2021-01-23 18:34:04 -0500

    update read.py to extract initial infections from excel and update dictionaries

--- read.py
+++ read.py
@@ -1,24 +1,37 @@
 from openpyxl import load_workbook
 #loading excel file
 wb2 = load_workbook('OEC2021-School_Record_Book.xlsx')
+tas = {}
+teachers = {}
+studentRecord = {}
 
-#This class creates a dictonary with all infected students
-def infectionList():
-    #loading virus 
+#This method will update the student, TA and teacher dictionaries with the appropriate initial infections
+#No value is returned by this function as the function is simply used to update the initial infections into the "givenRisk" keys of the dictionaries
+def infectionList(ListofTeachers, ListofTAs, ListofStudents):
     sheet = wb2['ZBY1 Status']
-    infected = {}
-    j=0
     lastRow = sheet.max_row
+        
     for i in sheet["A2":"D"+str(lastRow)]:
-        infected[j] = {"StudentID": i[0].value, "Name": i[2].value+" "+i[1].value, "Infected": 1}
-        #print(infected[j])
-        j+=1
-    return infected
+        if(i[0].value == "N/A"):
+            TA_name = i[2].value+" "+i[1].value
+            for index in tas:
+                if(TA_name == tas[index]["Name"]):
+                    tas[index]["givenRisk"] = 1
+
+        elif(i[0].value > 20):
+            studentRecord[i[0].value]["givenRisk"] = 1
+
+        else:
+            teacherName = i[2].value+" "+i[1].value
+            if(teacherName == teachers[i[0].value]["Name"]):
+                teachers[index]["givenRisk"] = 1
+            else:
+                studentRecord[i[0].value]["givenRisk"] = 1
 
 #This class creates a dictonary with all teachers
 def teacherList():
     sheet = wb2['Teacher Records']
-    teachers = {}
+    global teachers
     lastRow = sheet.max_row
     for i in sheet["A2":"D"+str(lastRow)]:
         teachers[i[0].value] = {"TeacherNumber": i[0].value, "Name": i[2].value+" "+i[1].value, "Class": i[3].value, "givenRisk": 0}
@@ -27,8 +40,8 @@ def teacherList():
 
 #This class creates a dictonary with all teaching assistants
 def TAList():
+    global tas
     sheet = wb2['Teaching Assistant Records']
-    tas = {}
     j=0
     lastRow = sheet.max_row
     for i in sheet["A2":"F"+str(lastRow)]:
@@ -39,15 +52,12 @@ def TAList():
 
 def studentList():
     sheet = wb2['Student Records']
-    studentRecord = {}
+    global studentRecord
     for i in sheet["A2":"J581"]:
         studentRecord[i[0].value] = {"StudentID": i[0].value, "Name": i[2].value+" "+i[1].value, "Grade": i[3].value, "ClassP1": i[4].value, "ClassP2": i[5].value, "ClassP3": i[6].value, "ClassP4": i[7].value, "healthFlag": 0 if(i[8].value == "N/A") else 1, "ECs": i[9].value.split(',')[0], "givenRisk": 0}
     
-    sheet = wb2['ZBY1 Status']
-    lastRow = sheet.max_row
-    for i in sheet["A2":"D"+str(lastRow)]:
-        studentRecord[]
     return studentRecord
+
 
 def putStudentsInClass():
     classesP1 = dict()
@@ -84,4 +94,4 @@ def putStudentsInClass():
     AllClasses['ClassP4'] = classesP4
     return AllClasses 
 
-print(studentList())
+print(infectionList(teacherList(), TAList(), studentList()))
